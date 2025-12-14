26a4b41642f9deb48618a2579d56dc39d616dcf4
GymNoLimitFitness <joseantoniomoreno22000099@gmail.com>
2025-12-14 23:05:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById("formulario").addEventListener("submit", async function(
   const datos = Object.fromEntries(new FormData(e.target));
 
   const respuesta = await fetch(https://hook.eu1.make.com/nu47yh9emkoujexk7r1yr6hfo3nbm68g
+
 , {
     method: "POST",
     headers: { "Content-Type": "application/json" },
